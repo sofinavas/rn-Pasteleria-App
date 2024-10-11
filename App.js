c13132794afbf5622e36995614b74a0c7f3a1e77
sofinavas <sofianavasd@gmail.com>
2024-10-10 23:03:50 -0300

    agregando session con SQLite

--- App.js
+++ App.js
@@ -1,14 +1,23 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 
-import { useState } from "react";
+import { colors } from "./src/global/colors";
 import { useFonts } from "expo-font";
 import { fonts } from "./src/global/fonts";
-import Navigator from "./src/navigation/Navigator";
+import MainNavigatorNavigator from "./src/navigation/MainNavigator";
 import { store } from "./src/app/store";
 import { Provider } from "react-redux";
+import { init } from "./src/db";
 
 export default function App() {
+
+  init()
+  .then(()=> console.log("DB inicializada"))
+  .catch(err => {
+    console.log("Initialization DB failed:");
+    console.log(err.message)
+  })
+
   const [fontsLoaded] = useFonts(fonts);
 
   if (!fontsLoaded) {
@@ -18,9 +27,9 @@ export default function App() {
   return (
     <>
       <Provider store={store}>
-        <Navigator />
+        <MainNavigator />
       </Provider>
-      <StatusBar style="auto" />
+      <StatusBar style="light" backgroundColor={colors.fucsia} />
     </>
   );
 }
