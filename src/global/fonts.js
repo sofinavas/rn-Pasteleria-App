e20699d67fb51aec68918ab840d41ba9dcd4a9ec
sofinavas <sofianavasd@gmail.com>
2024-10-11 20:13:08 -0300

    agrego estilos a los copmponentes

--- src/global/fonts.js
+++
@@ -1,8 +0,0 @@
-export const fonts = {
-  DancingBold: require("../../assets/fonts/dancing/DancingScript-Bold.ttf"),
-  DancingMedium: require("../../assets/fonts/dancing/DancingScript-Medium.ttf"),
-  MontserratBold: require("../../assets/fonts/montserrat/Montserrat-Bold.ttf"),
-  MontserratExtraLight: require("../../assets/fonts/montserrat/Montserrat-ExtraLight.ttf"),
-  MontserratLight: require("../../assets/fonts/montserrat/Montserrat-Light.ttf"),
-  Playwright: require("../../assets/fonts/playwright/PlaywriteESDeco-Regular.ttf"),
-};
